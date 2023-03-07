9a9ad34f72195066534e48fa856b70a2b0ac0121
naackisnotavailable <nroney101@gmail.com>
2023-03-06 21:03:44 -0500

    iy

---
+++ tele/funcs/autonComms.py
@@ -0,0 +1,120 @@
+import ctre
+import math
+cc = 6*math.pi
+class moveCm(object):
+    def __init__(self, l1, l2, r1, r2, inches):
+            self.inches = inches
+            self.ticks = 1
+
+            self.l1 = l1
+            self.l2 = l2
+            self.r1 = r1
+            self.r2 = r2
+
+            self.l1.setSelectedSensorPosition(0, 0, 0)
+            self.l2.setSelectedSensorPosition(0, 0, 0)
+            self.r1.setSelectedSensorPosition(0, 0, 0)
+            self.r2.setSelectedSensorPosition(0, 0, 0)
+
+            self.inte_last = 0
+
+
+
+    def main(self):
+            
+            rot = self.inches / cc
+        
+            self.ticks = rot * 9650
+
+            self.l1.set(ctre._ctre.ControlMode.Position, self.ticks)
+            self.l2.set(ctre._ctre.ControlMode.Position, self.ticks)
+            
+            self.r1.set(ctre._ctre.ControlMode.Position, self.ticks)
+            self.r2.set(ctre._ctre.ControlMode.Position, self.ticks)
+    def checkCompletion(self, l1, r1):
+        if l1.getSelectedSensorPosition() > self.ticks - 100 and r1.getSelectedSensorPosition() > self.ticks - 100:
+              return True
+        else:
+              return False
+def moveOut(grab, grabEncoder, lift, liftEncoder, io, ioEncoder, exPID, autonSwiv, grabby):
+    curr = grabby.getOutputCurrent() / 100
+
+
+    grabby.set(0.55 - curr)  #grab cone
+    
+    
+    io.set(exPID.main(ioEncoder, True))  #intake out
+    
+    
+    if liftEncoder.getPosition() > -60: #lift start going up
+        lift.set(-0.4)
+    else:
+        lift.set(0.0)
+    
+    
+    if liftEncoder.getPosition() > -55:  #swivel control
+        grab.set(-0.1)
+    else:
+        autonSwiv.main(grabEncoder.getPosition(), grab, 21)
+    
+
+    if grabEncoder.getPosition() > 8: #late lift control movements
+        if liftEncoder.getPosition() < -60 and liftEncoder.getPosition() > -75:
+            lift.set(-0.25)
+        else:
+            lift.set(0.0)
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+        #    
+#
+#
+#
+        #if liftEncoder.getPosition() < -70 and grabbyEncoder.getPosition() > 4:
+        #    print('opening')
+        #    grabby.set(-0.2 + curr)
+#
+#
+        ##NEED TO CHECK WHEN EXTENDED, THEN PLACE
+#
+        ##part 2; move everything back in // NOT WORKING PROBABLY
+#
+        #if False == True:
+        #    curr = grabby.getOutputCurrent() / 100
+        #    print('SWIVEL ENCODER: ' + str(grabEncoder.getPosition()))
+#
+#
+        #    print('running t1')
+        #    print('closing')
+        #    grabby.set(0.4 - curr)
+        #    io.set(exPID.main(ioEncoder, True)) # intake moves out
+        #    print('liftpos: ' + str(liftEncoder.getPosition()))
+        #    grab.set(-0.1)
+        #    if grabEncoder.getPosition() < -0.5:
+        #        print('running t2')
+        #        if liftEncoder.getPosition() < -5: #lift begins moving
+        #            print('running t3')
+        #            lift.set(0.2)
+        #        else:
+        #            lift.set(0.0)
+        #    else:
+        #        lift.set(0.0)
+        #
+        ## After moving back in, drive directly backward for currently undetermined distance
+        ## After leaving community, drive up to charge station and engage PID until end of autonomous
+        #if False == True:
+        #    spinPID(gyro.getYaw(), leftMotors, rightMotors, 180)
--- tele/robot.py
+++ tele/robot.py
@@ -17,7 +17,7 @@ from funcs import autoBalance as balancePID
 from funcs import spinPID as spinPID
 from funcs import __init__ as initialize
 from funcs import swivelPA
-
+from funcs import autonComms as aCs
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
         #self.camera = CameraServer.startAutomaticCapture()
@@ -60,112 +60,21 @@ class Robot(wpilib.TimedRobot):
         self.autonSwiv = swivelPA.PID()
         self.called = 0
 
+
         functions.setGPos(self.grabEncoder)
 
+        self.moveBack1 = aCs.moveCm(self.leftTalon1, self.leftTalon2, self.rightTalon1, self.rightTalon2, -23)
 
 
-    def autonomousPeriodic(self):  #test just drivetrain movement before anything else.
-
-        def moveCm(rot):
-            self.called += 1
-            
-            if self.called == 1:
-                self.leftTalon1.setSelectedSensorPosition(0, 0, 0)
-                self.leftTalon2.setSelectedSensorPosition(0, 0, 0)
-                self.rightTalon1.setSelectedSensorPosition(0, 0, 0)
-                self.rightTalon2.setSelectedSensorPosition(0, 0, 0)
-
-            ticks = rot * 9550
-
-            self.leftTalon1.set(ctre._ctre.ControlMode.Position, ticks)
-            self.leftTalon2.set(ctre._ctre.ControlMode.Position, ticks)
-            
-            self.rightTalon1.set(ctre._ctre.ControlMode.Position, ticks)
-            self.rightTalon2.set(ctre._ctre.ControlMode.Position, ticks)
-
+    def autonomousPeriodic(self):
+        self.moveBack1.main()
+        if self.moveBack1.checkCompletion == True:
+            self.stage1C = aCs.moveOut(self.grab, self.grabEncoder, self.lift, self.liftEncoder, self.io, self.ioEncoder, self.exPID, self.autonSwiv, self.grabby)
         
-
-        print('grab pos: ' + str(self.grabEncoder.getPosition()))
-        moveCm(1)
-
-        print('active')
-
-        curr = self.grabby.getOutputCurrent() / 100
-
+        if self.stage1C == True:
+            aCs.moveIn()
+            
         
-
-        # part one; place preloaded cube
-
-        # Move 24 in back from start to place preloaded cube, CHECK DIRECTION BEFORE TESTING
-
-
-        #print('closing')
-        #self.grabby.set(0.55 - curr)
-#
-        #self.io.set(self.exPID.main(self.ioEncoder, True)) # intake moves out
-#
-#
-        #print('liftpos: ' + str(self.liftEncoder.getPosition()))
-#
-        #if self.liftEncoder.getPosition() > -60: #lift begins moving
-        #    self.lift.set(-0.4)
-        #else:
-        #    self.lift.set(0.0)
-#
-#
-#
-        #if self.liftEncoder.getPosition() > -55:
-        #    self.grab.set(-0.1)
-        #else:
-        #    self.autonSwiv.main(self.grabEncoder.getPosition(), self.grab, 21)
-#
-#
-        #
-        #if self.grabEncoder.getPosition() > 8:
-        #    if self.liftEncoder.getPosition() < -60 and self.liftEncoder.getPosition() > -75:
-        #        self.lift.set(-0.25)
-        #    else:
-        #        self.lift.set(0.0)
-        #
-#
-        #    
-#
-#
-#
-        #if self.liftEncoder.getPosition() < -70 and self.grabbyEncoder.getPosition() > 4:
-        #    print('opening')
-        #    self.grabby.set(-0.2 + curr)
-#
-#
-        ##NEED TO CHECK WHEN EXTENDED, THEN PLACE
-#
-        ##part 2; move everything back in // NOT WORKING PROBABLY
-#
-        #if False == True:
-        #    curr = self.grabby.getOutputCurrent() / 100
-        #    print('SWIVEL ENCODER: ' + str(self.grabEncoder.getPosition()))
-#
-#
-        #    print('running t1')
-        #    print('closing')
-        #    self.grabby.set(0.4 - curr)
-        #    self.io.set(self.exPID.main(self.ioEncoder, True)) # intake moves out
-        #    print('liftpos: ' + str(self.liftEncoder.getPosition()))
-        #    self.grab.set(-0.1)
-        #    if self.grabEncoder.getPosition() < -0.5:
-        #        print('running t2')
-        #        if self.liftEncoder.getPosition() < -5: #lift begins moving
-        #            print('running t3')
-        #            self.lift.set(0.2)
-        #        else:
-        #            self.lift.set(0.0)
-        #    else:
-        #        self.lift.set(0.0)
-        #
-        ## After moving back in, drive directly backward for currently undetermined distance
-        ## After leaving community, drive up to charge station and engage PID until end of autonomous
-        #if False == True:
-        #    self.spinPID(self.gyro.getYaw(), self.leftMotors, self.rightMotors, 180)
 #
     def teleopPeriodic(self):
         self.slowed = functions.drive(self.leftTalon1,
